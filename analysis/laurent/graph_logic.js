a4d453cbbde575322c73dcb1c97aa289d4ed47f3
Laurent <laurent.faucher@polymtl.ca>
2023-04-23 20:03:39 +0200

    LF: almost done with first CO2 viz

--- analysis/laurent/graph_logic.js
+++ analysis/laurent/graph_logic.js
@@ -6,6 +6,160 @@ function whenDocumentLoaded(action) {
 		action();
 	}
 }
+
+let JSON_RAW_CO2 = {}
+
 whenDocumentLoaded(() => {
-	// load the data
+	fetch('./raw_CO2.json')
+		.then((response) => response.json())
+		.then((raw_CO2) => {
+			JSON_RAW_CO2 = raw_CO2;
+			const foodList = Object.keys(raw_CO2).sort((a, b) => 0.5 - Math.random());
+			container = document.getElementById('slider-container');
+			loadList(container, foodList)
+		})
 });
+
+function CO2ToData(itemInfo) {
+	const data = []
+	Object.keys(itemInfo).forEach((key) => {
+		if (itemInfo[key] > 0 && key !== 'total') 
+			data.push({name: itemInfo[key] >= 0.05 ? key: '', value: itemInfo[key]})
+	});
+	console.log(data);
+	return data;
+}
+
+function plot_donut_chart(event) {
+	const id = event.target.id;
+	const itemInfo = JSON_RAW_CO2[id]
+	console.log(itemInfo);
+	data = CO2ToData(itemInfo)
+	const  container = document.getElementById('pie-chart');
+	container.innerHTML = "";
+
+	title = document.createElement('h2');
+	title.appendChild(document.createTextNode(`${id}`));
+	container.appendChild(title);
+
+	child = DonutChart(data, {
+		name: d => d.name,
+		value: d => d.value,
+	})
+	container.appendChild(child)
+
+	const facts = document.createElement('h3');
+	facts.appendChild(document.createTextNode(`Total: ${itemInfo['total']} Kg CO2/Kg`));
+	container.appendChild(facts);
+}
+
+function getClassName(id) {
+	const total = JSON_RAW_CO2[id]['total']
+	if (total < 1.51) return 'label label-success slider-item';
+	else if (total < 5.1) return 'label label-warning slider-item';
+	else return 'label label-danger slider-item'
+}
+
+function loadList(container_element, food_array) {
+	container_element.innerHTML = "";
+	food_array.forEach(e => {
+		const newDiv = document.createElement('div');
+		const newFood = document.createTextNode(e.length > 25 ? `${e.slice(0, 25)} ...`: e);
+		newDiv.appendChild(newFood);
+		newDiv.className = getClassName(e);
+		newDiv.id = e;
+		newDiv.addEventListener("click", plot_donut_chart, false);
+		container_element.appendChild(newDiv)
+	});
+}
+
+// Copyright 2021 Observable, Inc.
+// Released under the ISC license.
+// https://observablehq.com/@d3/donut-chart
+function DonutChart(data, {
+	name = ([x]) => x,  // given d in data, returns the (ordinal) label
+	value = ([, y]) => y, // given d in data, returns the (quantitative) value
+	title, // given d in data, returns the title text
+	width = 640, // outer width, in pixels
+	height = 400, // outer height, in pixels
+	innerRadius = Math.min(width, height) / 3, // inner radius of pie, in pixels (non-zero for donut)
+	outerRadius = Math.min(width, height) / 2, // outer radius of pie, in pixels
+	labelRadius = (innerRadius + outerRadius) / 2, // center radius of labels
+	format = ",", // a format specifier for values (in the label)
+	names, // array of names (the domain of the color scale)
+	colors, // array of colors for names
+	stroke = innerRadius > 0 ? "none" : "white", // stroke separating widths
+	strokeWidth = 1, // width of stroke separating wedges
+	strokeLinejoin = "round", // line join of stroke separating wedges
+	padAngle = stroke === "none" ? 1 / outerRadius : 0, // angular separation between wedges
+  } = {}) {
+	// Compute values.
+	const N = d3.map(data, name);
+	const V = d3.map(data, value);
+	const I = d3.range(N.length).filter(i => !isNaN(V[i]));
+  
+	// Unique the names.
+	if (names === undefined) names = N;
+	names = new d3.InternSet(names);
+  
+	// Chose a default color scheme based on cardinality.
+	if (colors === undefined) colors = d3.schemeSpectral[names.size];
+	if (colors === undefined) colors = d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), names.size);
+  
+	// Construct scales.
+	const color = d3.scaleOrdinal(names, colors);
+  
+	// Compute titles.
+	if (title === undefined) {
+	  const formatValue = d3.format(format);
+	  title = i => `${N[i]}\n${formatValue(V[i])}`;
+	} else {
+	  const O = d3.map(data, d => d);
+	  const T = title;
+	  title = i => T(O[i], i, data);
+	}
+  
+	// Construct arcs.
+	const arcs = d3.pie().padAngle(padAngle).sort(null).value(i => V[i])(I);
+	const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+	const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
+	
+	const svg = d3.create("svg")
+		.attr("width", width)
+		.attr("height", height)
+		.attr("viewBox", [-width / 2, -height / 2, width, height])
+		.attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+  
+	svg.append("g")
+		.attr("stroke", stroke)
+		.attr("stroke-width", strokeWidth)
+		.attr("stroke-linejoin", strokeLinejoin)
+	  .selectAll("path")
+	  .data(arcs)
+	  .join("path")
+		.attr("fill", d => color(N[d.data]))
+		.attr("d", arc)
+	  .append("title")
+		.text(d => title(d.data));
+  
+	svg.append("g")
+		.attr("font-family", "sans-serif")
+		.attr("font-size", 10)
+		.attr("text-anchor", "middle")
+	  .selectAll("text")
+	  .data(arcs)
+	  .join("text")
+		.attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+	  .selectAll("tspan")
+	  .data(d => {
+		const lines = `${title(d.data)}`.split(/\n/);
+		return (d.endAngle - d.startAngle) > 0.25 ? lines : lines.slice(0, 1);
+	  })
+	  .join("tspan")
+		.attr("x", 0)
+		.attr("y", (_, i) => `${i * 1.1}em`)
+		.attr("font-weight", (_, i) => i ? null : "bold")
+		.text(d => d);
+  
+	return Object.assign(svg.node(), {scales: {color}});
+  }
