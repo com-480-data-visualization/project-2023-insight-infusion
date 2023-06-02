e9dd4ab4a3070d258cb86156c687174c50d3b09b
Rein Gundersen Bentdal <rein.bent@gmail.com>
2023-06-02 12:05:59 +0200

    map info minor

--- website/src/js/charts/map/map_info.js
+++ website/src/js/charts/map/map_info.js
@@ -6,10 +6,10 @@ export const viewCountryInfo = (countryName, transportationLength = undefined, m
   const infoElement = document.getElementById("map-info")
   const infoTitle = document.getElementById("map-info-name")
   const infoLength = document.getElementById("map-info-length")
-
+  
   if (infoTitle.innerHTML != countryName || infoLength.innerHTML == "") {
     infoTitle.innerHTML = countryName
-  
+    
     if (transportationLength != undefined) {
       infoLength.innerHTML = `Typical transportation length of ${transportationLength.toFixed(0)} km`
     } else {
@@ -19,15 +19,17 @@ export const viewCountryInfo = (countryName, transportationLength = undefined, m
     infoElement.style.opacity = 1
   }
   
-
-
+  
+  
   if (viewTimer != null) {
     clearTimeout(viewTimer)
     viewTimer = null
   }
+  
+  const length_changed = transportationLength != undefined || infoTitle.innerHTML != countryName && infoLength.innerHTML == ""
 
   viewTimer = setTimeout(() => {
     infoElement.style.opacity = 0
     viewTimer = null
-  }, 1000)
+  }, length_changed ? 9999999 : 1000)
 }
